172e2be4d094a100a2503d35826c54b344df38da
mtlevi70 <levente.hegyi@stud.uni-corvinus.hu>
2024-12-03 02:16:49 +0100

    KÉSZ AHHAHHAHHA

--- rand_spotify.js
+++ rand_spotify.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", rand_spotify());
 
 function rand_spotify(){
     embbed = document.querySelectorAll(`.embbed`);
+    console.log(embbed.length);
     let rand_numb = Math.floor(Math.random() * (embbed.length));
-    embbed[rand_numb].style.opacity = '1';
+    embbed[rand_numb].style.display = 'block';
 }
